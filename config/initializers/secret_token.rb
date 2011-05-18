0427df44d9a384294f0e948c664bc3cccc3d594a
Eyal Gersht <eyal@gersht.com>
2011-05-18 14:19:45 +0300

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieQuotes::Application.config.secret_token = '6963096fa2a0e03b171b87a35dc3ca24de2ce025a371fc99b1527d7432dd0a32db26df0d3807218ff745a00c921b79c28a35864c896941b67e7436ba6eca4631'
